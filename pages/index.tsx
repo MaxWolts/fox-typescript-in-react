a406479061a720689f9824eba5964e5c9042a741
Max <max.wolts@gmail.com>
2023-05-22 20:33:43 -0300

    :recycle: types for events and callbacks of listeners

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import RandomFox from "@/components/RandomFox";
+import type {MouseEventHandler} from "react";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 //generate simple unique id
@@ -11,16 +12,19 @@ type ImageItem = {
 }
 
 export default function Home() {
-  const [images, setImages] = useState<ImageItem[]>([
-    {id: `${generateId()}`, url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: `${generateId()}`, url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: `${generateId()}`, url: `https://randomfox.ca/images/${random()}.jpg`},
-    {id: `${generateId()}`, url: `https://randomfox.ca/images/${random()}.jpg`},
-  ]);
+  const [images, setImages] = useState<ImageItem[]>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    const newImageItem: ImageItem = {id: `${generateId()}`, url: `https://randomfox.ca/images/${random()}.jpg`}
+    setImages([...images, newImageItem]);
+  }
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello</h1>
+      <button onClick={addNewFox}>Add new fox</button>
       {images.map(image => 
         <div key={image.id}>
           <RandomFox image={image.url} />
